b91bcd131abb32e9e6c185e8a6360f801f218d6e
Chaoran Lu <andyluloveusc@gmail.com>
2024-04-11 15:32:18 -0700

    Creating views and view logic

--- chefsTable/myapp/views.py
+++ chefsTable/myapp/views.py
@@ -1,5 +1,17 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from datetime import datetime
 
-def home(request):
-    return HttpResponse("Welcome to Little Lemon Restaurant!")
+def say_hello(request):
+    return HttpResponse("Hello World")
+
+def homepage(request):
+    return HttpResponse("Welcome to Little Lemon!")
+
+def display_date(request):
+    date_joined = datetime.today().year
+    return HttpResponse(date_joined)
+
+def menu(request):
+    text = """<h1 style="color: #F4CE14;"> This is Little Lemon again! </h1>"""
+    return HttpResponse(text)
