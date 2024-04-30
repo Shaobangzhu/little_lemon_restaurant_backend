6aee41f9a3996fa037e745e24fe467a0fd5ae750
Chaoran Lu <andyluloveusc@gmail.com>
2024-04-30 15:07:49 -0700

    beginning of creating request and responses

--- chefsTable/myapp/views.py
+++ chefsTable/myapp/views.py
@@ -1,17 +1,2 @@
 from django.shortcuts import render
 from django.http import HttpResponse
-from datetime import datetime
-
-def say_hello(request):
-    return HttpResponse("Hello World")
-
-def homepage(request):
-    return HttpResponse("Welcome to Little Lemon!")
-
-def display_date(request):
-    date_joined = datetime.today().year
-    return HttpResponse(date_joined)
-
-def menu(request):
-    text = """<h1 style="color: #F4CE14;"> This is Little Lemon again! </h1>"""
-    return HttpResponse(text)
