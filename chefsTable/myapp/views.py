c4913ccb505a829d8e69acd69973e8a8004e1879
Chaoran Lu <andyluloveusc@gmail.com>
2024-04-30 15:56:48 -0700

    Show request object information to the home page

--- chefsTable/myapp/views.py
+++ chefsTable/myapp/views.py
@@ -1,2 +1,20 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+
+def home(request):
+    path = request.path
+    scheme = request.scheme
+    method = request.method
+    address = request.META['REMOTE_ADDR']
+    user_agent = request.META['HTTP_USER_AGENT']
+    path_info = request.path_info
+    
+    msg = f"""<br>
+            <br>Path: {path}
+            <br>Address: {address}
+            <br>Scheme: {scheme}
+            <br>Method: {method}
+            <br>User agent: {user_agent}
+            <br>Path info: {path_info}
+    """
+    return HttpResponse(msg, content_type='text/html', charset='utf-8')
