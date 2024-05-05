0335fea9fd56cee6054a8b6d820e0c2db857a743
Chaoran Lu <andyluloveusc@gmail.com>
2024-05-05 11:34:36 -0700

    Mapping URLs with Params

--- chefsTable/myapp/views.py
+++ chefsTable/myapp/views.py
@@ -1,24 +1,13 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 
-def home(request):
-    path = request.path
-    scheme = request.scheme
-    method = request.method
-    address = request.META['REMOTE_ADDR']
-    user_agent = request.META['HTTP_USER_AGENT']
-    path_info = request.path_info
-    
-    response = HttpResponse()
-    response.headers['Age'] = 20
-    
-    msg = f"""<br>
-            <br>Path: {path}
-            <br>Address: {address}
-            <br>Scheme: {scheme}
-            <br>Method: {method}
-            <br>User agent: {user_agent}
-            <br>Path info: {path_info}
-            <br>Response header: {response.headers}
-    """
-    return HttpResponse(msg, content_type='text/html', charset='utf-8')
+def menuitems(request, dish):
+        items = {
+                'pasta': 'Pasta is a type of noodle made from combination of wheat, water or eggs',
+                'falafel': 'Falafel are deep fried patties or balls made from broad beans, ground chickpeas, or both',
+                'cheesecake': 'Cheesecake is a type of dessert made with cream, soft cheese on top of cookie, pastry crust or graham cracker and fruit sauce topping'
+        }
+        
+        description = items[dish]
+        
+        return HttpResponse(f"<h2> {dish} </h2>" + description)
