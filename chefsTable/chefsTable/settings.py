4efe1712a7b1e7ce4f033f64c1a4c207fd51bf94
Chaoran Lu <andyluloveusc@gmail.com>
2024-05-10 10:24:16 -0700

    re-init project

--- chefsTable/chefsTable/settings.py
+++ chefsTable/chefsTable/settings.py
@@ -31,7 +31,6 @@ ALLOWED_HOSTS = ['*']
 # Application definition
 
 INSTALLED_APPS = [
-    'menuapp.apps.MenuappConfig',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
--- chefsTable/chefsTable/urls.py
+++ chefsTable/chefsTable/urls.py
@@ -20,7 +20,6 @@ from . import views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', include('myapp.urls')),
     path('home/', views.home),
 ]
 
