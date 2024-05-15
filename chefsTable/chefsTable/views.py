e81a74e82ca35467ebb5084aed17d222586b5bbe
Chaoran Lu <andyluloveusc@gmail.com>
2024-05-15 09:43:29 -0700

    remove the chefstable project

--- chefsTable/chefsTable/views.py
+++
@@ -1,7 +0,0 @@
-from django.http import HttpResponse, HttpResponseNotFound
- 
-def handler404(request, exception):
-    return HttpResponse("404 Page not Found! <br><br> <button onclick="" href='';""> Go to Homepage")
-
-def home(request):
-    return HttpResponse(" Little Lemon ! ")
