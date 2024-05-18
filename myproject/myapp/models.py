b8526cb5dbcd06fd486f4aa07e41451c39e051ad
Chaoran Lu <andyluloveusc@gmail.com>
2024-05-18 11:01:49 -0700

    clean up migration

--- myproject/myapp/models.py
+++
@@ -1,7 +0,0 @@
-from django.db import models
-
-# Create your models here.
-class Menuitems(models.Model):
-    itemname = models.CharField(max_length=200)
-    category = models.CharField(max_length=300)
-    year = models.IntegerField()
