458aa16da4c82063bea0a2e6db705c4046b383be
Chaoran Lu <andyluloveusc@gmail.com>
2024-05-24 16:54:25 -0700

    Creating Model Form

--- myproject/myapp/forms.py
+++ myproject/myapp/forms.py
@@ -1,13 +1,8 @@
 from django import forms
 
-SHIFTS = (
-    ("1", "Morning"),
-    ("2", "Afternoon"),
-    ("3", "Evening"),
-)
+from .models import Logger
 
-class InputForm(forms.Form):
-    first_name = forms.CharField(max_length = 200, required=False)
-    last_name = forms.CharField(max_length = 200)
-    shift = forms.ChoiceField(choices = SHIFTS)
-    time_log = forms.TimeField(help_text = "Enter the exact time")
+class LogForm(forms.ModelForm):
+    class Meta:
+        model = Logger
+        fields = '__all__'
