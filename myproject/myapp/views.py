4440a1820dc2ec22491e969827b6228cb7821e75
Chaoran Lu <andyluloveusc@gmail.com>
2024-05-24 15:53:13 -0700

    Create a form

--- myproject/myapp/views.py
+++ myproject/myapp/views.py
@@ -1,3 +1,9 @@
 from django.shortcuts import render
+from django.http import HttpResponse
 
-# Create your views here.
+from myapp.forms import InputForm
+
+def form_view(request):
+    form = InputForm()
+    context = {"form": form}
+    return render(request, "home.html", context)
